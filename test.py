870d06e06240c79cac63664821efdc5fb9b7815b
budai <budai9902@gmail.com>
2022-05-09 19:37:06 +0200

    saving for keyboard, bigger plots

--- test.py
+++ test.py
@@ -18,15 +18,17 @@ def Patient(fileName):
     # data1 = data1.dropna(how='any')
 
     l = []
+    global data1
+
     data1 = pd.read_csv(fileName, sep=',', encoding='latin1')
-    texts(data1.iloc[2,2], 5, 2)
-    texts(data1.iloc[0,2], 4, 2)
-    texts(data1.iloc[5, 2], 6, 2)
-    texts(data1.iloc[6, 2], 7, 2)
-    date = data1.iloc[1,2]
+    texts(data1.iloc[2, 2], 5, 2)  # eye
+    texts(data1.iloc[0, 2], 4, 2)  # birthday
+    texts(data1.iloc[5, 2], 6, 2)  # flash
+    texts(data1.iloc[6, 2], 7, 2)  # flash color
+    date = data1.iloc[1, 2]
 
     cString = data1.columns[2]
-    texts(cString, 3, 2)
+    texts(cString, 3, 2)  # patient ID
 
     i = data1.loc[data1['PatientID'] == 'Reported Waveform'].index.values
     j = data1.loc[data1['PatientID'] == 'Raw Waveform'].index.values
@@ -47,6 +49,7 @@ def Patient(fileName):
 
     print(data4.columns)
     print(data4)
+
     #  plotting
 
     data2.plot(x=f'{cString}', y='Unnamed: 3')
@@ -78,22 +81,29 @@ def Patient(fileName):
     plt.savefig('Unnamed11.1.png')
     p = plot(f'{cString}.4', date, 'Unnamed11.1.png')
     l.append(p)
-    # plt.show()
 
     data4.plot(x=f'{cString}.13', y=f'{cString}.15')
     plt.savefig('Unnamed15.png')
     p = plot(f'{cString}.13', date, 'Unnamed15.png')
     l.append(p)
-    # plt.show()
-    return l
+    return l, data1
 
 
 def texts(s, r, c, cs=1):
+    """
+    This is a helper function for creating text labels
+    :parameter: s is the string, r is the row number, c is the column number, cs is the offset --> default is 1
+    :rtype: object
+    """
     label = tk.Label(text=s)
     label.grid(row=r, column=c, columnspan=cs)
 
 
 def main_texts(filename):
+    """
+    This function is responsible for the main Gui appearance, before loading any csv file
+    :rtype: object
+    """
     row, col = 3, 1
     # texts
     texts("Patient ID:", row, col)
@@ -107,49 +117,53 @@ def main_texts(filename):
     texts("Flash color:", row, col)
     row += 1
 
-    texts("Plot name:", 9, 4)
+    texts("Plot name:", 12, 4)
     row += 1
-    texts("Date:", 10, 4)
+    texts("Date:", 13, 4)
     row += 1
 
     my_font2 = ('times', 12, 'bold')
-    l1 = tk.Label(window, text='Press N for normal \n Press D for diseased', width=20, font=my_font2)
+    l1 = tk.Label(window, text='Press N for normal \n Press D for diseased\n Press C for corrupt data', width=20, font=my_font2)
     l1.grid(row=2, column=0)
-    """
-    OPTIONS = [
-        "Evaluate",
-        "Normal",
-        "Disease",
-        "data corrupt"
-    ]
-    variable = tk.StringVar(window)
-    variable.set(OPTIONS[0])  # default value
-    w = tk.OptionMenu(window, variable, *OPTIONS)
-    w.grid(row=5, column=0)
-    """
-    save = tk.Button(window, text='Save evaluation',
-                   width=20, command=lambda: save_result())
-    save.grid(row=4, column=0)
 
     b2 = tk.Button(window, text='Previous',
                    width=20, command=lambda: prev_img())
-    b2.grid(row=9, column=3 )
+    b2.grid(row=12, column=3)
 
     b1 = tk.Button(window, text='Next',
                    width=20, command=lambda: next_img())
-    b1.grid(row=9, column=8)
+    b1.grid(row=12, column=8)
 
 
 def save_result(event):
-    f_types = [('CSV Files', '*.csv')]
-    filename = filedialog.askopenfilename(filetypes=f_types)
-    data1 = pd.read_csv(filename, sep=',', encoding='latin1')
+    """
+    Saving result into the file for button click and for "N" or "D" keyboard press
+    :rtype: object
+    """
+    try:
+        if len(filename) == 0:
+            return
+    except:
+        return
+    c = filename.find("_")
+    print(c)
+    fname = filename[c:]
+
     if event.char == "n":
-        print("saving..."+ event.char+ " Normal")
-        data1[0][15]="N"
+        print("saving..." + event.char + " Normal")
+        data1["Diagnoses"] = "NORMAL"
+        data1.to_csv("saved_file" + fname, index=False)
+
+
     elif event.char == "d":
         print("saving..." + event.char + " Disease")
-        data1[0][15] = "D"
+        data1["Diagnoses"] = "DISEASE"
+        data1.to_csv("saved_file.csv", index=False)
+
+    elif event.char == "c":
+        print("saving..." + event.char + " Data corrupt")
+        data1["Diagnoses"] = "DATA CORRUPT"
+        data1.to_csv("saved_file.csv", index=False)
 
 
 def prev_img():
@@ -172,16 +186,13 @@ def upload_file():
     global img
     global l
     global idx
+    global data1
     idx = 0
-    # f_types = [('Jpg Files', '*.jpg')]
-    # filename = filedialog.askopenfilename(filetypes=f_types)
-    # img = ImageTk.PhotoImage(file=filename)
-    # b2 = tk.Button(window, image=img)  # using Button
-    # b2.grid(row=3, column=3, rowspan=6, columnspan=6)
 
     f_types = [('CSV Files', '*.csv')]
+    global filename
     filename = filedialog.askopenfilename(filetypes=f_types)
-    l = Patient(filename)
+    l, data1 = Patient(filename)
     draw_img()
     main_texts(filename)
 
@@ -193,10 +204,9 @@ def draw_img():
     image_file_location = l[idx].filename
     img = ImageTk.PhotoImage(file=image_file_location)
     b2 = tk.Button(window, image=img)  # using Button
-    b2.grid(row=3, column=3, rowspan=6, columnspan=6)
-    texts(l[idx].name, 9, 5, 3)
-    texts(l[idx].date, 10, 5, 3)
-
+    b2.grid(row=3, column=3, rowspan=8, columnspan=8)
+    texts(l[idx].name, 12, 5, 3)
+    texts(l[idx].date, 13, 5, 3)
 
 
 window = tk.Tk()
@@ -209,7 +219,6 @@ window.title('gui')
 my_font1 = ('times', 18, 'bold')
 l1 = tk.Label(window, text='Select a patient\'s file', width=30, font=my_font1)
 l1.grid(row=1, column=1)
-
 
 # button
 b1 = tk.Button(window, text='Upload File',
